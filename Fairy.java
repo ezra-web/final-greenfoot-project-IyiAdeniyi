4fb4668fe12c9ce9ae2df5aa468af42dc832144f
IyiAdeniyi <340925593@gapps.yrdsb.ca>
2022-12-21 23:05:43 -0500

    titleScreen

--- Fairy.java
+++ Fairy.java
@@ -76,5 +76,6 @@ public class Fairy extends Actor
         }
         
         animateFairy();
+
     }
 }
--- TitleScreen.java
+++ TitleScreen.java
@@ -16,6 +16,16 @@ public class TitleScreen extends World
     public TitleScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(800, 450, 1); 
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        PlayButton playButton = new PlayButton();
+        addObject(playButton,415,335);
     }
 }
